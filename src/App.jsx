8ae49e2ec31c4c20916fcaade2f75f62c6caa6ca
Hridoy-shill <ridoyaaa60@gmail.com>
2023-04-30 22:01:04 +0600

    file ready for develop

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import img from '../src/assets/Rectangle 1.png'
+
+function App() {
+
+
+  return (
+    <>
+      <p className='text-6xl'>this is staring</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBD2Cm3DnPkEbGTj0BCJDovqS-WWJ0eZyc",
+  authDomain: "travel-guru-79a7c.firebaseapp.com",
+  projectId: "travel-guru-79a7c",
+  storageBucket: "travel-guru-79a7c.appspot.com",
+  messagingSenderId: "99928765906",
+  appId: "1:99928765906:web:8f217c97e2a112155e440b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
